e8b5fb3f4460d3b3b7ce520da9a65e281105d3f8
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-09-24 13:23:25 +0200

    prebuilt bindings for win

--- dev-building-bindings.sh
+++ dev-building-bindings.sh
@@ -6,13 +6,15 @@ array=( playerbase roadmanager scenarioengine  controller )
 for MYMODULE in "${array[@]}"
 do
 	echo $MYMODULE
-	rm -rf $PWD/esmini/src/$MYMODULE ;  mkdir  $PWD/esmini/src/$MYMODULE
-	~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/src/$MYMODULE --bind $MYMODULE  --single-file  --config $PWD/esmini/src/py$MYMODULE.cfg  $PWD/esmini/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
+	rm -rf $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+	mkdir  $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+	~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/EnvironmentSimulator/src/$MYMODULE --bind $MYMODULE  --single-file  --config $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.cfg  $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
 done
 
 MYMODULE="viewerbase"
-rm -rf $PWD/esmini/src/$MYMODULE ;  mkdir  $PWD/esmini/src/$MYMODULE
-~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/src/$MYMODULE --bind viewer --single-file  --config $PWD/esmini/src/py$MYMODULE.cfg  $PWD/esmini/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
+rm -rf $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+mkdir  $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/EnvironmentSimulator/src/$MYMODULE --bind viewer --single-file  --config $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.cfg  $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
 
 
 ## no binding
@@ -20,6 +22,7 @@ array=( esminilib esminirmlib )
 for MYMODULE in "${array[@]}"
 do
 	echo $MYMODULE
-	rm -rf $PWD/esmini/src/$MYMODULE ;  mkdir  $PWD/esmini/src/$MYMODULE
-	~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/src/$MYMODULE --bind ""  --single-file  --config $PWD/esmini/src/py$MYMODULE.cfg  $PWD/esmini/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
+	rm -rf $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+	mkdir  $PWD/esmini/EnvironmentSimulator/src/$MYMODULE
+	~/binder/bin/binder --annotate-includes --trace --root-module py$MYMODULE --prefix $PWD/esmini/EnvironmentSimulator/src/$MYMODULE --bind ""  --single-file  --config $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.cfg  $PWD/esmini/EnvironmentSimulator/src/py$MYMODULE.hpp  -- -std=c++14 -DNDEBUG $IPATHS
 done
