b60061305bdc8a324e227eea9257a28d30ca9bbe
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-11-22 16:30:01 +0100

    pybind11::return_value_policy::referenc

--- intermodule.py
+++ intermodule.py
@@ -8,34 +8,39 @@ import time
 print(dir(pyplayerbase.ScenarioPlayer))
 print("Python: running constructor")
 player = pyplayerbase.ScenarioPlayer(["filename", "--window",  "60", "60", "800", "400",  "--osc", "./esmini/resources/xosc/cut-in.xosc"])
-odr = player.GetODRManager()
+
 se= player.scenarioEngine
-sg= player.scenarioGateway
+
 
 print("scenarioEngine", se)
 print("scenarioEngine", dir(se))
+while not player.IsQuitRequested():
+    print("x")
+    player.Frame(1)
+    print("GetParameterValueInt", player.GetParameterValueInt("test",1))
+    print("scenarioEngine method:", se.GetHeadstartTime())
 
-exit()
 
+
+
+
+odr = player.GetODRManager()
 print("GetODRManager", odr)
 print("GetODRManager", dir(odr))
+exit()
 road = odr.GetRoadById(0)
-print("GetODRManager::", road)
+print("road::", road)
 print("road.Print()::")
 road.Print()
 
 
+
+sg= player.scenarioGateway
 print("scenarioGateway", sg)
 print("scenarioGateway", dir(sg))
-print("scenarioEngine method:", se.GetHeadstartTime())
 
 
 
-while not player.IsQuitRequested():
-    print("x")
-    player.Frame(1)
-    time.sleep(1)
-    print("GetParameterValueInt", player.GetParameterValueInt("test",1))
 
-print("Done")
+
 
